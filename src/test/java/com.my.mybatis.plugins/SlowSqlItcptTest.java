522521155adeaffaa6ab2aa5326b4ff1ee9d0afa
helloworldwt <971759990@qq.com>
2016-12-29 17:08:10 +0800

    create slow sql test update mybatis xml

--- src/test/java/com.my.mybatis.plugins/SlowSqlItcptTest.java
+++ src/test/java/com.my.mybatis.plugins/SlowSqlItcptTest.java
@@ -1,8 +1,22 @@
 package com.my.mybatis.plugins;
 
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.my.mybatis.plugins.dao.UserMapper;
+
 /**
  * @Author: wangtian
  */
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:spring/spring.xml")
 public class SlowSqlItcptTest {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    //case slow sql
 }
